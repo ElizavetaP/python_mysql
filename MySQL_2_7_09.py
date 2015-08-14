a28ceed48c485da40330c324a25d153b1034c492
Liza <lizap@bk.ru>
2015-08-14 18:50:12 +0400

    first commit

--- MySQL_2_7_09.py
+++ MySQL_2_7_09.py
@@ -1,85 +1,117 @@
 import MySQLdb
 import sys
 
-
-if not len(sys.argv)==3:
-    print("Write: station filename")
-    sys.exit(1)
-    
 db = MySQLdb.connect(host="localhost", user="root", passwd = "surf", db = "db")
 cursor = db.cursor()
-    
-if sys.argv[2] == 'None':
-    a = []
-    while len(a) != 5:
-        a = input('Write: date temperature pressure nebulosity humidity:  ')
-        a = a.split()
-    argv = a
-    
-    print argv
+
+if sys.argv[1] == 'import':
+    if len(sys.argv)==4:
         
+        def unpack_line(line):
+            argv = line.split(';')
+            return argv
 
+        f = open(sys.argv[3], "r")
+        lines = f.readlines()
 
-    sql = """INSERT INTO prognoz1(date, temperature, pressure, nebulosity, humidity, station)
-    VALUES ('%(date)s', '%(temperature)s', '%(pressure)s', '%(nebulosity)s', '%(humidity)s', '%(station)s')
-    """%{"date":argv[0], "temperature":argv[1], "pressure":argv[2], "nebulosity":argv[3], "humidity":argv[4], "station":sys.argv[1]}
-    cursor.execute(sql)
-    db.commit()
+        for line in lines:
+            argv = unpack_line(line)
+            sql = """INSERT INTO prognoz1(date, temperature, pressure, nebulosity, humidity, station)
+            VALUES ('%(date)s', '%(temperature)s', '%(pressure)s', '%(nebulosity)s', '%(humidity)s', '%(station)s')
+            """%{"date":argv[0], "temperature":argv[1], "pressure":argv[2], "nebulosity":argv[3], "humidity":argv[4], "station":sys.argv[2]}
+            cursor.execute(sql)
+            db.commit()
+
+    else:
+        print("Write: regime station filename")
+        sys.exit(1)
+
+if sys.argv[1] == 'avarage':
+    if len(sys.argv)==4:
+        sql2 = """SELECT temperature from prognoz1 WHERE date BETWEEN '%(1)s' AND '%(2)s'
+        """%{"1":sys.argv[2], "2":sys.argv[3]}
+        cursor.execute(sql2)
+        data = cursor.fetchall()
+
+        t = 0
+        n = 0
+
+        for rec in data:
+            t = t + int(rec[0])
+            n = n + 1
+        print 'avarage temperature:', t/n
+
+    else:
+        print("Write: regime first date last date")
+        sys.exit(1)
+
+########avarage for day###############
+def daycalc(st,param):
     
-else:
-    def unpack_line(line):
-        argv = line.split(';')
-        return argv
+    sql3 = """SELECT %(1)s from prognoz1 WHERE station = '%(2)s' AND date = CAST(sysdate() AS DATE) 
+    """%{"1":param, "2":st}
+    cursor.execute(sql3)
 
-    f = open(sys.argv[1], "r")
-    lines = f.readlines()
+    data = cursor.fetchall()
 
-    for line in lines:
-        argv = unpack_line(line)
-        sql = """INSERT INTO prognoz1(date, temperature, pressure, nebulosity, humidity, station)
-        VALUES ('%(date)s', '%(temperature)s', '%(pressure)s', '%(nebulosity)s', '%(humidity)s', '%(station)s')
-        """%{"date":argv[0], "temperature":argv[1], "pressure":argv[2], "nebulosity":argv[3], "humidity":argv[4], "station":sys.argv[1]}
-        cursor.execute(sql)
-        db.commit()
-    
+    t = 0
+    n = 0
 
-##print('avarage temperature')
-##first = input('first date: ')
-##last = input('last date: ')
-##sql2 = """SELECT temperature from prognoz1 WHERE date BETWEEN '%(1)s' AND '%(2)s'
-##"""%{"1":first, "2":last}
-##cursor.execute(sql2)
-##
-##data = cursor.fetchall()
-##print(data)
-##
-##t = 0
-##n = 0
-##
-##for rec in data:
-##    t = t + int(rec[0])
-##    n = n + 1
-##print(t/n)
+    for rec in data:
+        t = t + int(rec[0])
+        n = n + 1
+    return(t/n)
+######################################
 
+if sys.argv[1] == 'daycalc':
+    if len(sys.argv)==4:
+        print 'day-calculate for a station', daycalc(sys.argv[2],sys.argv[3])
+    else:
+        print("Write: regime station parameter")
+        sys.exit(1)
+        
+##########print all###################
+def allst():
+    sql4 = """SELECT station from prognoz1 WHERE date = CAST(sysdate() AS DATE) 
+    """
+    cursor.execute(sql4)
+    data = cursor.fetchall()
+    allst = []
+    for rec in data:
+        if rec[0] not in allst:
+            allst.append(rec[0])
+    return allst
+            
+def printall(st):
+    sql5 = """SELECT * from prognoz1 WHERE station = '%(1)s' AND date = CAST(sysdate() AS DATE) 
+    """%{"1":st}
+    cursor.execute(sql5)
 
-print('day-calculate for a station')
-st = input('station name: ')
-param = input('parameter: ')
-sql3 = """SELECT %(1)s from prognoz1 WHERE station = '%(2)s' AND date = CAST(sysdate() AS DATE) 
-"""%{"1":param, "2":st}
-cursor.execute(sql3)
-print sql3
+    data = cursor.fetchall()
+    for rec in data:
+        print 'station', rec[5]
+        print 'date', rec[0]
+        print 'temperature', rec[1]
+        print 'pressure', rec[2]
+        print 'nebulosity', rec[3]
+        print 'humidity', rec[4]   
+#########################################
+        
+if sys.argv[1] == 'hottest':
 
-data = cursor.fetchall()
-print(data)
+    t = -100
+    for st in allst():
+        tst = daycalc(st,'temperature')
+        if tst > t:
+            maxst = st
+            t = tst
+    print 'hottest satation:', maxst
+    printall(maxst)
 
-t = 0
-n = 0
+if sys.argv[1] == 'all':
+    for st in allst():
+        printall(st)
 
-for rec in data:
-    t = t + int(rec[0])
-    n = n + 1
-print(t/n)
 
 
 db.close()
